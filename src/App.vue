31efcaab6138b23af1308b9cfaba3c489af0c65d
andreas.stephan <andreas.stephan@internetx.com>
2019-08-05 17:53:54 +0200

    zwischenstand

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,43 @@
 <template>
-  <div id="app" style="margin: 20px 0;">
-    <daterange-picker
-      locale="de"
-      submit-title="Ok"
-      begin="2016-02-19T00:00:00.000+01:00"
-    />
-  </div>
+    <div id="app" style="margin: 20px 0;">
+        <daterange-picker
+                locale="de"
+                submit-title="Ok"
+                from="2019-08-04T14:46:58.570Z"
+                to="2019-08-05T14:46:58.570Z"
+                begin="2016-02-19T00:00:00.000+01:00"
+                @update="checkUpdate"
+                @reset="checkReset"
+        />
+    </div>
 </template>
 
 <script>
-import DaterangePicker from "./components/DaterangePicker";
+    import DaterangePicker from './components/DaterangePicker'
 
-export default {
-  name: "app",
-  components: {
-    DaterangePicker
-  }
-};
+    export default {
+        name: 'app',
+        components: {
+            DaterangePicker
+        },
+        methods: {
+
+            checkUpdate (values) {
+                console.info('update', values)
+            },
+            checkReset (values) {
+                console.info('reset', values)
+            }
+        }
+    }
+
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  padding: 5vw;
-}
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        padding: 5vw;
+    }
 </style>
